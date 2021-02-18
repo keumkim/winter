9467d0633961b850be1ec6e699db0846c243ba55
keumkim <77368991+keumkim@users.noreply.github.com>
2021-02-18 12:18:39 +0900

    Add files via upload

---
+++ Coupon/src/main/java/winterschoolone/PolicyHandler.java
@@ -0,0 +1,54 @@
+package winterschoolone;
+
+import winterschoolone.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+    
+    @Autowired
+    CouponRepository couponRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverAssigned_(@Payload Assigned assigned){ //stampQty 증가
+    	
+    	if(assigned.isMe()){
+        	Optional<Coupon> optional = couponRepository.findById(assigned.getOrderId());
+        	if(optional != null && optional.isPresent())
+        	{
+        		Coupon coupon = optional.get();
+        		
+        		// stamp 추가 
+        		coupon.setStampQty(coupon.getStampQty() + assigned.getQty());
+        		
+        		while(true) {
+        			if(coupon.getStampQty()>=10) { //10개 이상일 경우 Coupon지급
+        				coupon.setCouponQty(coupon.getCouponQty() + 1);
+        				coupon.setStampQty(coupon.getStampQty() - 10);
+        			}
+        			else { 
+        				break;
+        			}			
+        		}
+        		
+        		couponRepository.save(coupon);
+        	}
+            
+            System.out.println("##### listener  : " + assigned.toJson());
+        }
+    }
+  
+}
